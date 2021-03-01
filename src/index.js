24517f983cb8b0df4b5a17206fb4e495724435be
Clement Gaston <clem31130@hotmail.fr>
2021-03-01 21:50:20 +0100

    :sparkles: Adding basic JSX transformation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-document.getElementById("root").innerText = "Initial structure";
+import TinyReact from '../lib/tinyReact';
+
+const element = (
+	<div id='foo'>
+		<h1>Hello !</h1>
+		<p>Here you can render some basic JSX !</p>
+		<p>Everything is interactive !</p>
+	</div>
+);
+
+const container = document.getElementById('root');
+TinyReact.render(element, container);
