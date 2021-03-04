7a77d3e9c4776816a29fe560f9d817ebb5fc2dcd
Clement Gaston <clem31130@hotmail.fr>
2021-03-04 17:25:56 +0100

    :sparkles: Add Event Listener to JSX translation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import TinyReact from '../lib/tinyReact';
 
-const element = (
-	<div id='foo'>
-		<h1>Hello !</h1>
-		<p>Here you can render some basic JSX !</p>
-		<p>Everything is interactive !</p>
-	</div>
-);
-
 const container = document.getElementById('root');
-TinyReact.render(element, container);
+
+const updateValue = e => {
+	rerender(e.target.value);
+};
+
+const rerender = value => {
+	const element = (
+		<div id='foo'>
+			<h1>What's your name ?</h1>
+			<input onInput={updateValue} value={value} />
+			<p>Your name is : {value}</p>
+		</div>
+	);
+	TinyReact.render(element, container);
+};
+
+rerender('');
